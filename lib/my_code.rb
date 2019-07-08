b401cdf6a0660e2e3d2b8eccec2ad06356049ed9
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-08 20:27:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,7 @@ end
 def reduce_to_total(source_array, starting_point = 0)
   array_total = 0
   count = starting_point
-  while count < source_array.length
+  while count < source_array.length do
     array_total = array_total + source_array[count].to_i
     count += 1
     return array_total
