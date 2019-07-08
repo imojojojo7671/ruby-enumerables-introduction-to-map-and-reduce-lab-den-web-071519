ba5d547378ca18d42636253f42f467c0992ac486
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-08 21:10:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,3 +39,17 @@ def reduce_to_total(source_array, starting_point = 0)
     end
   return array_total
 end
+
+def reduce_to_all_true(source_array)
+  truthy_array = 1
+  source_array.length.times { |index|
+    if source_array[index] == TRUE
+      truthy_array += 1
+    end  
+  }
+  truthy_array == source_array.length ? return TRUE
+  
+end
+
+
+  
