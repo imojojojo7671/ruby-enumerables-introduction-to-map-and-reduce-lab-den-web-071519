8f6ffd8249bb770cafc8962da1fea504ca982061
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-08 21:57:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ end
 def reduce_to_all_true(source_array)
   source_array.length.times { |index|
     if source_array[index] == true
-    else return "falsey return"
+    else puts "falsey return"
     end
     }
 end
